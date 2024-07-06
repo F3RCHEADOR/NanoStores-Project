0ee21131558793aadbb0c97fb46b96327f090a40
Fabian F <noobseca123@hotmail.com>
2024-07-06 10:39:01 -0500

    version 1

---
+++ src/data/links.ts
@@ -0,0 +1,19 @@
+export interface Links {
+  title: string,
+  path: string
+}
+
+export const linksData: Links[] = [
+  {
+    title: "empresas", path: "/"
+  },
+  {
+    title: "Developers", path: "/developers"
+  },
+  {
+    title: "Blog", path: "/blog/"
+  },
+  {
+    title: "empleos", path: "/empleos/"
+  }
+]
